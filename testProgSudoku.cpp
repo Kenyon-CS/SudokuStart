3494c2dbadefd5fda533cd3b4e235fd9134c458a
James Skön <skonjp@kenyon.edu>
2024-10-08 10:58:36 -0400

    Create testProgSudoku.cpp

---
+++ testProgSudoku.cpp
@@ -0,0 +1,16 @@
+// No work has been done beyond the source code given in the book
+// Please include the standard info about yourself and the program here
+
+#include <iostream>
+#include <cmath>
+#include "sudoku.h"
+
+using namespace std;
+
+int main()
+{
+
+    cout << "See Programming Exercise 18." << endl;
+
+    return 0;
+}
